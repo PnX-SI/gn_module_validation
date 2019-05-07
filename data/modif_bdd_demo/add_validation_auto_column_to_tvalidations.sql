e80fb240ee3ea73b11c19cdd8d6ccc8cc6327217
Camille Monchicourt <camille.monchicourt@ecrins-parcnational.fr>
2019-05-07 17:33:37 +0200

    Clean unecessary files

--- data/modif_bdd_demo/add_validation_auto_column_to_tvalidations.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE gn_commons.t_validations ADD COLUMN validation_auto boolean;
-ALTER TABLE gn_commons.t_validations ALTER COLUMN validation_auto SET DEFAULT true;
-COMMENT ON COLUMN gn_commons.t_validations.validation_auto IS 'Définir si la validation est manuelle ou automatique';
-UPDATE gn_commons.t_validations SET validation_auto = true;
-ALTER TABLE gn_commons.t_validations ALTER COLUMN validation_auto SET NOT NULL;
